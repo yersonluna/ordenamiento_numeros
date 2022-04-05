5d847a07e58c29860880d36735e8e2e981ea707a
Yerson Andres Luna <70661133+yersonluna@users.noreply.github.com>
2022-04-05 17:20:44 -0500

    Add files via upload

---
+++ Ingesoftware/src/main/java/com/mycompany/ingesoftware/LeerArchivo.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ingesoftware;
+
+/**
+ *
+ * @author yerso
+ */
+public class LeerArchivo {
+    public static void main(String[]args){
+        Menu ven = new Menu();
+        ven.setVisible(true);
+    }
+    
+}
